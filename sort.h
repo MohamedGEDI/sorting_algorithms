eb5390280707353e77be3193cb7aedc29e879406
MohamedGEDI <gedimohammed@gmail.com>
2021-10-26 13:30:56 -0700

    fix

--- sort.h
+++ sort.h
@@ -22,4 +22,7 @@ void bubble_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void realqs(int *array, int start, int end, size_t size);
+int partition(int *array, size_t start, size_t end, size_t size);
 #endif
